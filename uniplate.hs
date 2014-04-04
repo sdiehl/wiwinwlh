7c649a86d0c13d1562f3e5e18f7009c6ebf2a893
Stephen Diehl <stephen.m.diehl@gmail.com>
2014-04-04 13:31:47 -0500

    add uniplate cnf

--- uniplate.hs
+++ uniplate.hs
@@ -3,7 +3,7 @@ import Data.Generics.Uniplate.Direct
 data Expr a
   = Fls
   | Tru
-  | Lit a
+  | Var a
   | Not (Expr a)
   | And (Expr a) (Expr a)
   | Or (Expr a) (Expr a)
@@ -23,5 +23,37 @@ simplify = transform simp
    simp (Not Tru) = Fls
    simp x = x
 
-test =
-  simplify (Not (Not (Not (Not (Lit "a"))))) == Lit "a"
+reduce :: Show a => Expr a -> Expr a
+reduce = rewrite cnf
+  where
+    -- double negation
+    cnf (Not (Not p))        = Just $ p
+
+    -- de Morgan
+    cnf (Not (p `Or` q))     = Just $ (Not p) `And` (Not q)
+    cnf (Not (p `And` q))    = Just $ (Not p) `Or` (Not q)
+
+    -- distribute conjunctions
+    cnf (p `Or` (q `And` r)) = Just $ (p `Or` q) `And` (p `Or` r)
+    cnf ((p `And` q) `Or` r) = Just $ (p `Or` q) `And` (p `Or` r)
+    cnf _ = Nothing
+
+
+example1 :: Expr String
+example1 = simplify (Not (Not (Not (Not (Var "a")))))
+-- Var "a"
+
+example2 :: [String]
+example2 = [a | Var a <- universe ex]
+  where
+    ex = Or (And (Var "a") (Var "b")) (Not (And (Var "c") (Var "d")))
+-- ["a","b","c","d"]
+
+example3 :: Expr String
+example3 = reduce $ ((a `And` b) `Or` (c `And` d)) `Or` e
+  where
+    a = Var "a"
+    b = Var "b"
+    c = Var "c"
+    d = Var "d"
+    e = Var "e"
