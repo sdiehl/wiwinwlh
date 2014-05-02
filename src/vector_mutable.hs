b47b4eb00e3b859ad9ea1723da1162eff8eea6ce
Stephen Diehl <stephen.m.diehl@gmail.com>
2014-05-02 17:46:59 -0500

    better vector example

--- src/vector_mutable.hs
+++ src/vector_mutable.hs
@@ -1,14 +1,29 @@
 import GHC.Prim
 import Control.Monad
+import Control.Monad.ST
+import Control.Monad.Primitive
+
 import Data.Vector.Unboxed (freeze)
 import Data.Vector.Unboxed.Mutable
+import qualified Data.Vector.Unboxed as V
 
-example :: IO (MVector RealWorld Int)
+example :: PrimMonad m => m (V.Vector Int)
 example = do
   v <- new 10
   forM_ [0..9] $ \i ->
      write v i (2*i)
-  return v
+  freeze v
+
+-- vector computation in IO
+vecIO :: IO (V.Vector Int)
+vecIO = example
+
+-- vector computation in ST
+vecST :: ST s (V.Vector Int)
+vecST = example
+
 
 main :: IO ()
-main = example >>= freeze >>= print
+main = do
+  vecIO >>= print
+  print $ runST vecST
