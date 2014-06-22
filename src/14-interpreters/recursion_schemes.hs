5c8398b158e620c19c6292e05703750f4f384062
Stephen Diehl <stephen.m.diehl@gmail.com>
2014-06-21 23:07:24 -0500

    added recursion schemes

---
+++ src/14-interpreters/recursion_schemes.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DeriveFunctor #-}
+
+import Data.Functor.Foldable
+
+type Var = String
+
+data Exp
+  = Var Var
+  | App Exp Exp
+  | Lam [Var] Exp
+  deriving Show
+
+data ExpF a
+  = VarF Var
+  | AppF a a
+  | LamF [Var] a
+  deriving Functor
+
+type instance Base Exp = ExpF
+
+instance Foldable Exp where
+  project (Var a)      = VarF a
+  project (App a b)    = AppF a b
+  project (Lam a b)   = LamF a b
+
+instance Unfoldable Exp where
+  embed (VarF a)      = Var a
+  embed (AppF a b)    = App a b
+  embed (LamF a b)    = Lam a b
+
+fvs :: Exp -> [Var]
+fvs = cata phi
+  where phi (VarF a)    = [a]
+        phi (AppF a b)  = a ++ b
+        phi (LamF a b) = foldr (filter . (/=)) a b
