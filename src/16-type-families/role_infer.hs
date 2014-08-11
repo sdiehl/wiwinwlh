c8d6bf05865cac2254158d1e6e620fa79d11fb70
Stephen Diehl <stephen.m.diehl@gmail.com>
2014-08-11 13:34:52 -0500

    added poly-kinded roles

---
+++ src/16-type-families/role_infer.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE RoleAnnotations #-}
+
+data Nat = Zero | Suc Nat
+
+type role Vec nominal representational
+data Vec :: Nat -> * -> * where
+  Nil  :: Vec Zero a
+  (:*) :: a -> Vec n a -> Vec (Suc n) a
+
+type role App representational nominal
+data App (f :: k -> *) (a :: k) = App (f a)
+
+type role Mu nominal nominal
+data Mu f a = Roll (f (Mu f) a)
+
+type role Proxy phantom
+data Proxy a = Proxy
