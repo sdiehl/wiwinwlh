bdd5c785640b8c44161f3bdced9a61c21574f264
Kenneth Foner <kenny.foner+github@gmail.com>
2014-12-10 20:13:34 -0500

    Update impredicative.hs
    Added explanation that only the second example requires impredicativity.

--- src/11-quantification/impredicative.hs
+++ src/11-quantification/impredicative.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE ImpredicativeTypes #-}
 
+-- Uses higher-ranked polymorphism.
 f :: (forall a. [a] -> a) -> (Int, Char)
 f get = (get [1,2], get ['a', 'b', 'c'])
 
+-- Uses impredicative polymorphism.
 g :: Maybe (forall a. [a] -> a) -> (Int, Char)
 g Nothing = (0, '0')
 g (Just get) = (get [1,2], get ['a','b','c'])
