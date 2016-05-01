6c388dddf077c59de0e596676246ea8c3775602d
Arianna Morgan <rmorgan@trystero.is>
2016-05-01 13:42:50 -0700

    Add functions to src/01-basics/bottoms.hs
     - Added the new example code from the bottoms section in
       the main tutorial.md document.

--- src/01-basics/bottoms.hs
+++ src/01-basics/bottoms.hs
@@ -1,6 +1,16 @@
 import GHC.Err
 import Prelude hiding (head, (!!), undefined)
 
+-- Annotated code that features use of the error function.
+
+error :: String -> a                       -- Takes an error message of type
+                                           -- String and returns what ever type
+                                           -- is needed
+
+divByY:: (Num a, Eq a, Fractional a) => a -> a -> a
+divByY _ 0 = error "Divide by zero error"      -- Dividing by 0 causes an error
+divByY dividend divisor = dividend / divisor   -- Handles defined division
+
 -- degenerate functions
 
 undefined :: a
