717a038911fb796ada86d896cd79b32b17de95e0
Arianna Morgan <rmorgan@trystero.is>
2016-05-23 18:50:07 -0700

    Add comments to example1

--- src/02-monads/maybe.hs
+++ src/02-monads/maybe.hs
@@ -1,8 +1,10 @@
 example1 :: Maybe Int
 example1 = do
-  a <- Just 3
-  b <- Just 4
-  return $ a + b
+  a <- Just 3                -- Bind 3 to name a
+  b <- Just 4                -- Bind 4 to name b
+  return $ a + b             -- Evaluate (a + b), then use 'return' to ensure
+                             -- the result is in the Maybe monad in order to
+                             -- satisfy the type signature
 -- Just 7
 
 example2 :: Maybe Int
