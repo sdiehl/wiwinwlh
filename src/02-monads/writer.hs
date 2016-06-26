f02e08eedf3477f60afdd76319e9e69c63101d31
Daniel Taskoff <daniel.taskoff@gmail.com>
2016-06-26 10:32:57 +0300

    Edit some of the examples for Monads and Monad Transformers

--- src/02-monads/writer.hs
+++ src/02-monads/writer.hs
@@ -4,9 +4,10 @@ type MyWriter = Writer [Int] String
 
 example :: MyWriter
 example  = do
-  tell [1..5]
-  tell [5..10]
+  tell [1..3]
+  tell [3..5]
   return "foo"
 
 output :: (String, [Int])
 output = runWriter example
+-- ("foo", [1, 2, 3, 3, 4, 5])
