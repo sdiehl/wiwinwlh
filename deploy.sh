0123b45acbb05d1bf4c2fdef3180069d195d1cb4
Stephen Diehl <stephen.m.diehl@gmail.com>
2016-11-06 22:24:17 +0000

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 rsync css/style.css ec2:~
 ssh ec2 'sudo mv style.css /srv/http/hask/css/style.css'
 
-rsync css/sidebar ec2:~
-ssh ec2 'sudo mv sidebar.css /srv/http/hask/css/sidebar.css'
+rsync css/layout.css ec2:~
+ssh ec2 'sudo mv sidebar.css /srv/http/hask/css/layout.css'
+
+rsync js/nav.js ec2:~
+ssh ec2 'sudo mv nav.js /srv/http/hask/nav.js'
 
 rsync tutorial.html ec2:~
 ssh ec2 'sudo mv tutorial.html /srv/http/hask/index.html'
