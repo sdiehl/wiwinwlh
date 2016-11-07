512e9ce8c095cdf7d738d43d2b05a7f483d5a774
Oskar Wickström <oskar.wickstrom@gmail.com>
2016-11-07 12:00:16 +0100

    Navigation and scroll fixes (#143)
    * Fix scrolling and some other minor goodies
    
    * Add document title and version at beginning
    
    * Fix rest of overflowing tables
    
    * Automatically wrap tables

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,7 @@
 $(function () {
+
+  // NAV
+
   var $sidebar = $('#sidebar-wrapper');
   var $toggle = $('#toggle-sidebar');
 
@@ -6,7 +9,13 @@ $(function () {
     $sidebar.toggleClass('expanded');
   });
 
-  $sidebar.find('.sidebar-nav').click(function () {
+  $sidebar.find('.sidebar-nav a').click(function () {
     $sidebar.removeClass('expanded');
   });
+
+  // TABLES
+  $('#main table').each(function () {
+    var $table = $(this).wrap('<div class="overflow-table"></div>');
+  });
+
 });
