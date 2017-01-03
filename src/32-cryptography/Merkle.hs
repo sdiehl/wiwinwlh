acf41f079a09bb92af0d136f3d5b85f0a2b3a805
Stephen Diehl <stephen.m.diehl@gmail.com>
2017-01-03 12:44:46 +0000

    merkle tree and bump project resolver

---
+++ src/32-cryptography/Merkle.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Crypto.Hash
+import Data.ByteArray (convert)
+import qualified Data.ByteString as B
+
+segmentSize :: Int
+segmentSize = 64
+
+type Hash = Digest SHA256
+
+joinHash :: Hash -> Hash -> Hash
+joinHash a b = hash (B.append (convert a) (convert b))
+
+segments :: B.ByteString -> [B.ByteString]
+segments bs
+  | B.null bs = []
+  | otherwise = seg : segments rest where
+    (seg, rest) = B.splitAt segmentSize bs
+
+merkleRoot :: [Hash] -> Hash
+merkleRoot [h] = h
+merkleRoot hs  = joinHash (merkleRoot left) (merkleRoot right)
+  where
+    (left, right) = splitAt i hs
+    i = until (\x -> x*2 >= length hs) (*2) 1
+
+tree :: Digest SHA256
+tree = merkleRoot $ map hash (["4", "8", "15", "16", "23", "42"] :: [B.ByteString])
+
+file :: IO ()
+file = B.readFile "Merkle.hs" >>= print . merkleRoot . map hash . segments
