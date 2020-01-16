d429c2e21b9636cffa27a8d4f063644b8bcecf1a
Igal Tabachnik <hmemcpy@gmail.com>
2020-01-16 16:49:01 +0200

    Fixing missing unicode characters in rendered PDF (#177)
    * Tweaking fonts to support unicode characters + making it pretty
    
    * Adding unicode mapping to math symbols for missing characters
    
    * Trying older ghc to fix the CI build issues

--- shell.nix
+++ shell.nix
@@ -1,18 +1,22 @@
-{ nixpkgs ? <nixpkgs>, compiler ? "ghc843" }:
+{ pkgs ? import <nixpkgs> {}, compiler ? "ghc843" }: with pkgs;
 let
-  pkgs = import nixpkgs { };
   ghcWithDeps = pkgs.haskell.packages.${compiler}.ghcWithPackages
     ( ps: with ps; [ base pandoc containers ] );
   tex = with pkgs; texlive.combine {
     inherit (texlive)
       scheme-small
       xetex
+      newunicodechar
       ;
+  };
+  fontsConf = makeFontsConf {
+    fontDirectories = [ dejavu_fonts ];
   };
 in
   pkgs.stdenv.mkDerivation {
     name = "wiwinwlh-env";
     buildInputs = [ ghcWithDeps tex ];
+    FONTCONFIG_FILE = fontsConf;
     shellHook = ''
       export LANG=en_US.UTF-8
       eval $(egrep ^export ${ghcWithDeps}/bin/ghc)
