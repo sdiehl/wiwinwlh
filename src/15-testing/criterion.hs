14921134fe325f4f8a080a8f0cb7762d40a34bd7
sdiehl <stephen.m.diehl@gmail.com>
2020-02-19 16:39:31 +0000

    Chapter 15 CI

--- src/15-testing/criterion.hs
+++ src/15-testing/criterion.hs
@@ -1,29 +1,30 @@
 import Criterion.Main
-import Criterion.Config
 
 -- Naive recursion for fibonacci numbers.
 fib1 :: Int -> Int
 fib1 0 = 0
 fib1 1 = 1
-fib1 n = fib1 (n-1) + fib1 (n-2)
+fib1 n = fib1 (n -1) + fib1 (n -2)
 
 -- Use the De Moivre closed form for fibonacci numbers.
 fib2 :: Int -> Int
-fib2 x = truncate $ ( 1 / sqrt 5 ) * ( phi ^ x - psi ^ x )
+fib2 x = truncate $ (1 / sqrt 5) * (phi ^ x - psi ^ x)
   where
-      phi = ( 1 + sqrt 5 ) / 2
-      psi = ( 1 - sqrt 5 ) / 2
+    phi = (1 + sqrt 5) / 2
+    psi = (1 - sqrt 5) / 2
 
 suite :: [Benchmark]
-suite = [
-    bgroup "naive" [
-      bench "fib 10" $ whnf fib1 5
-    , bench "fib 20" $ whnf fib1 10
-    ],
-    bgroup "de moivre" [
-      bench "fib 10" $ whnf fib2 5
-    , bench "fib 20" $ whnf fib2 10
-    ]
+suite =
+  [ bgroup
+      "naive"
+      [ bench "fib 10" $ whnf fib1 5,
+        bench "fib 20" $ whnf fib1 10
+      ],
+    bgroup
+      "de moivre"
+      [ bench "fib 10" $ whnf fib2 5,
+        bench "fib 20" $ whnf fib2 10
+      ]
   ]
 
 main :: IO ()
