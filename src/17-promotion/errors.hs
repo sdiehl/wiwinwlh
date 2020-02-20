e21ee8658c76cb759005e87047b2d4383ca5fc92
sdiehl <stephen.m.diehl@gmail.com>
2020-02-20 10:52:09 +0000

    Chapter 17 CI

--- src/17-promotion/errors.hs
+++ src/17-promotion/errors.hs
@@ -4,14 +4,16 @@
 
 import GHC.TypeLits
 
-instance
-  -- Error Message
-  TypeError (Text "Equality is not defined for functions"
-  :$$:
-  (ShowType a :<>: Text " -> " :<>: ShowType b))
+instance-- Error Message
 
+  TypeError
+    ( Text "Equality is not defined for functions"
+        :$$: (ShowType a :<>: Text " -> " :<>: ShowType b)
+    ) =>
   -- Instance head
-  => Eq (a -> b) where (==) = undefined
+  Eq (a -> b)
+  where
+  (==) = undefined
 
 -- Fail when we try to equate two functions
 example = id == id
