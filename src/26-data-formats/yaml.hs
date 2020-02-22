3a57473f4079a9d215461656b856ce71caada911
sdiehl <stephen.m.diehl@gmail.com>
2020-02-22 18:33:56 +0000

    Chapter 26 CI

--- src/26-data-formats/yaml.hs
+++ src/26-data-formats/yaml.hs
@@ -1,37 +1,41 @@
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-import Data.Yaml
-
-import Data.Text (Text)
 import qualified Data.ByteString as BL
-
+import Data.Text (Text)
+import Data.Yaml
 import GHC.Generics
 
-data Invoice = Invoice
-  { invoice :: Int
-  , date :: Text
-  , bill :: Billing
-  } deriving (Show,Generic,FromJSON)
+data Invoice
+  = Invoice
+      { invoice :: Int,
+        date :: Text,
+        bill :: Billing
+      }
+  deriving (Show, Generic, FromJSON)
 
-data Billing = Billing
-  { address :: Address
-  , family :: Text
-  , given :: Text
-  } deriving (Show,Generic,FromJSON)
+data Billing
+  = Billing
+      { address :: Address,
+        family :: Text,
+        given :: Text
+      }
+  deriving (Show, Generic, FromJSON)
 
-data Address = Address
-  { lines :: Text
-  , city :: Text
-  , state :: Text
-  , postal :: Int
-  } deriving (Show,Generic,FromJSON)
+data Address
+  = Address
+      { lines :: Text,
+        city :: Text,
+        state :: Text,
+        postal :: Int
+      }
+  deriving (Show, Generic, FromJSON)
 
 main :: IO ()
 main = do
   contents <- BL.readFile "example.yaml"
-  let (res :: Either String Invoice) = decodeEither contents
+  let (res :: Either ParseException Invoice) = decodeEither' contents
   case res of
+    Left err -> print err
     Right val -> print val
-    Left err -> putStrLn err
