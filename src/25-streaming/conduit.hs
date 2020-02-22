fda024dcc6f1863465036228b03f1a5910388a63
sdiehl <stephen.m.diehl@gmail.com>
2020-02-22 09:23:21 +0000

    Fix Conduit depreccations

--- src/25-streaming/conduit.hs
+++ src/25-streaming/conduit.hs
@@ -1,26 +1,25 @@
 {-# LANGUAGE MultiWayIf #-}
 
-import Data.Conduit
 import Control.Monad.Trans
+import Data.Conduit
 import qualified Data.Conduit.List as CL
 
-source :: Source IO Int
-source = CL.sourceList [1..100]
+source :: ConduitT () Int IO ()
+source = CL.sourceList [1 .. 100]
 
-conduit :: Conduit Int IO String
+conduit :: ConduitT Int String IO ()
 conduit = do
   val <- await
-  liftIO $ print val
   case val of
     Nothing -> return ()
     Just n -> do
-      if | n `mod` 15 == 0 -> yield "FizzBuzz"
-         | n `mod` 5  == 0 -> yield "Fizz"
-         | n `mod` 3  == 0 -> yield "Buzz"
-         | otherwise       -> return ()
+      if  | n `mod` 15 == 0 -> yield "FizzBuzz"
+          | n `mod` 5 == 0 -> yield "Fizz"
+          | n `mod` 3 == 0 -> yield "Buzz"
+          | otherwise -> return ()
       conduit
 
-sink :: Sink String IO ()
+sink :: ConduitT String o IO ()
 sink = CL.mapM_ putStrLn
 
 main :: IO ()
